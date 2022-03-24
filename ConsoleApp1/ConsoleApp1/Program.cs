8c1558057ff7fd3e443193aecf6d72aec08164e6
gularasul <699evkp@code.edu.az>
2022-03-24 11:27:40 +0400

    CosoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+           
+           
+
+            
+        }
+    }
+    
+}
+abstract class Person 
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, string surname, int age)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Age = age;
+    }
+
+    public Person()
+    {
+    }
+}
+//SalaryOfHour(saatbaşı qazandığı maaş)
+//WorkingHour(30 gün ərzində neçə saat işlədiyi)
+//CalculateSalary() - Ümumi maaşı hesablayan method
+class Employee:Person
+{
+    internal object person;
+
+    public double SalaryOfHour {
+        get
+        {
+            return SalaryOfHour;
+        }
+        set
+        {
+            if (value >1)
+            {
+                SalaryOfHour = 1;
+            }
+        }
+    }
+
+    public int WorkingHour
+    {
+        get
+        {
+            return WorkingHour;
+        }
+        set
+        {
+            if (value>180)
+            {
+                WorkingHour = 180;
+            }
+        }
+    }
+
+
+
+    public void CalculateSalary()
+    {
+        double Cal = WorkingHour * SalaryOfHour;
+        Console.WriteLine("1 ay erzinde umumi maash");
+
+
+    }
+    public string CalculateSalary(double SalaryOfHour, int WorkingHour, int age)
+
+    {
+        if (age < 18) return "Ishleme huququnuz yoxdur!";
+        double Cal = WorkingHour * SalaryOfHour;
+        Console.WriteLine("1 ay erzinde umumi maash");
+
+        string text = $"Teyin etdiyiniz ayliq maash: {Cal} azn-dir.";
+        if (Cal < 250) text += "\nAyliq maash 250-den ashagi olmamalidir.";
+
+        if (WorkingHour / 22 > 8)
+        {
+            text = "Ayliq ortalama ish saati 8 saatdan artiq olmalaidir!!!";
+        }
+
+        return text;
+    }
+}
+class student : Person
+{
+    //    IQRank(IQ imtahanının nəticəsi (100 bal üzərindən))
+    //   LanguageRank(Dil imtahanının nəticəsi (100 bal üzərindən))
+
+
+    public int IQRank {
+        get
+        {
+            return 0;
+        }
+        set
+        {
+            if (true)
+            {
+
+            }
+        }
+            }
+   
+
+    //public void ExamResult()
+    //{
+    //    int resul = IQRank + LanguageRank;
+    //}
+
+
+}
---
+++ ConsoleApp1/ConsoleApp1/student.cs
@@ -0,0 +1,67 @@
+﻿class student : Person
+{
+    //    IQRank(IQ imtahanının nəticəsi (100 bal üzərindən))
+    //   LanguageRank(Dil imtahanının nəticəsi (100 bal üzərindən))
+
+    public int IQRank {
+        get
+        {
+            return IQRank;
+        }
+        set
+        {
+            if (value > 60)
+
+            {
+                value = IQRank;
+
+            }
+
+        }
+    }
+
+    public int LanguageRank
+    {
+        get
+        {
+            return LanguageRank;
+        }
+        set
+        {
+            if (value > 60)
+
+            {
+                value = LanguageRank;
+
+            }
+
+        }
+    }
+    public void ExamResult(int IqRank, int LanguageRank, int age)
+    {
+        if (true)
+        {
+
+        }
+    }
+
+    
+        
+       
+
+    
+
+//public string ExamResult(int IQRank, int LanguageRank, int age)
+//{
+//    if (age < 6 || age > 20) return "Siz shagird deyilsigiz.";
+
+//    int Great = IQRank + LanguageRank;
+//    string text = $"Siz imtahandan {Great} bal toplamisiz.";
+
+//    if (Great >= 120)
+//        text += "\nTEBRIKLER! SIZ IMTAHANDAN UGURLA KECDINIZ.";
+//    else
+//        text += "\nSiz kechid bali toplaya bilmesiz. Teesufki sinifde qaldiniz. ";
+
+//    return text;
+//}
